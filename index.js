d864009147c24f43e1980c9f30b7642a7c543f71
Your Name <niclas.lundstrom@iths.se>
2021-10-27 15:10:21 +0200

    get, post, delete

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const port = 3000
 
-app.get('/', function (req, res) {
-  res.send("Hello World!");
-});
 
-const server = app.listen(3000, function () { });
+app.get('/user', (req, res) => {
+  res.send('Hello User\n')
+})
+
+app.get('/user/:id', (req, res) => {
+  const id = req.params.id
+  res.send(`/gregger/id ${id}`)
+})
+
+app.post('/user', (req, res) => {
+  res.send(`post /user/id ${req.params}`)
+})
+
+app.delete('/user', (req, res) => {
+  res.send('delete /User/id')
+})
+
+
+
+app.listen(port, () => {
+  console.log(`tjenare hejsan http://localhost:${port}`)
+})
