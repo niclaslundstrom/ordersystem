23db14dd1feb598a6f8529c32e38923dc8814acb
Pob74 <zeljko.haberstok@iths.se>
2021-11-01 14:31:34 +0100

    mongoose install and test

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 //const express = require('express')
 const app = express()
 const port = 3000
-import express from 'express'
-import router from './routes/users.js'
-
+import express from "express"
+import router from "./routes/users.js"
+import mongoose from "mongoose"
 
 app.get("/", (req, res) => {
   res.send("hej")
 })
 
-app.use("/user", router);
+app.use("/user", router)
 app.post("/user", (req, res) => {
-  res.send('postar')
+  res.send("postar")
 })
+
+mongoose.connect(
+  "mongodb+srv://order:order123@cluster0.q5oir.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  () => console.log("connected to db maybe")
+)
 app.listen(port, () => {
   console.log(`tjenare hejsan http://localhost:${port}`)
 })
