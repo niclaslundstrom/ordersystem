145a4466c1ee2de35d416852d361f6e308b66480
Sofia Premuzaj <sofia.premuzaj@gmail.com>
2021-11-02 15:31:12 +0100

    curl POST function okey

--- model.js
+++ model.js
@@ -3,18 +3,20 @@ import mongoose from "mongoose"
 const UserSchema = new mongoose.Schema({
   FirstName: {
     type: String,
-    // required: true,
+    required: true,
   },
   LastName: {
     type: String,
-    // required: true,
+    required: true,
   },
   Adress: {
     type: String,
-    // required: true,
+    required: true,
   },
 }, { versionKey: false });
 
 const userModel = mongoose.model("userModel", UserSchema);
 
 export default userModel;
+
+
