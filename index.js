88b14284f86e7a96024ebc125d55694d768271ab
Sofia Premuzaj <sofia.premuzaj@gmail.com>
2021-11-11 10:02:45 +0100

    changed connection string

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ app.use('/orders', Order)
 
 
 
-//const dbconn = process.env.DBCONN || '<defult>'
+const dbconn = process.env.DBCONN || '<defult>'
 
 
-const dbconn = 'mongodb + srv://order:order123@cluster0.q5oir.mongodb.net/ordersystem?retryWrites=true&w=majority'
+//const dbconn = 'mongodb+srv://order:order123@cluster0.q5oir.mongodb.net/ordersystem?retryWrites=true&w=majority'
 mongoose.connect(dbconn, () => console.log('connected to db maybe'))
 
 const db = mongoose.connection
